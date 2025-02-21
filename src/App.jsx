e3d821924314befa5ad1d54d6743a333a79d6d8f
AMeenalosini <69825070+AMeenalosini@users.noreply.github.com>
2025-02-20 18:10:27 -0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import {puppyList} from './data.js'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+import './App.css'
+import './index.css'
+
+
+function App() {
+
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  console.log("puppyList: ", puppyList);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log()
+
+  return (
+    <div className="App">
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )} 
+
+      {
+        puppies.map((puppy) => {
+             return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>;
+           })
+       }
+    </div>
+  );
+}
+
+export default App
